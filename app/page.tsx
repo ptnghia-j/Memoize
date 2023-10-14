f6fe80d8945ecdd1cd462ad0e90a9ff656425ad2
Nghia T Phan <ptnghia@csu.fullerton.edu>
2023-10-14 16:24:50 -0700

    first commit & set up

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Button }from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+     <Button className="bg-blue-500 text-white"/>
+    </div>
+    
+  )
+}
