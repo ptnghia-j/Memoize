ad95292ce4cd62cb43b860f2e1411507c7ffe758
Nghia T Phan <ptnghia@csu.fullerton.edu>
2023-10-27 17:04:38 -0700

    merged landing page and schema1

--- src/app/(marketing)/_components/logo.tsx
+++ src/app/(marketing)/_components/logo.tsx
@@ -13,12 +13,23 @@ export const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
       <Image
-        src="/logo_light.png"
-        height="40"
-        width="40"
-        alt="Logo"
-      />
+         src="/logo_light.png"
+         height="60"
+         width="60"
+         alt="Logo"
+         className="dark:hidden"
+       />
 
+       <Image
+         src="/logo_dark.png"
+         height="60"
+         width="60"
+         alt="Logo"
+         className="hidden dark:block"
+       />
+       <p className={cn("font-thin rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 transition-all hover:translate-y-[2px] md:block dark:border-white", font.className)}>
+         Memoize
+       </p>
 
     </div>
   )
